fd25a7464f620b3012d7cd1dcf3e450da36f6ff8
Arthur Cabon <djarth2222@gmail.com>
2025-06-10 18:26:07 -0400

    wip: period selection

--- main.py
+++ main.py
@@ -21,10 +21,19 @@ from src.balance import (
 # region |---| Config
 
 FIRST_DAY = 6
-
 BUDGET = None
-PERIOD_START = datetime.strptime(f"{FIRST_DAY}/08/2025", "%d/%m/%Y")
-PERIOD_END = PERIOD_START + relativedelta(months=1) 
+
+TODAY = datetime.now()
+if "period_start" not in st.session_state :
+    month = datetime.strptime(f"{FIRST_DAY}/{TODAY.month}/{TODAY.year}", "%d/%m/%Y")
+    
+    if TODAY.day < FIRST_DAY :
+        st.session_state.period_start = month - relativedelta(month=1)
+    else :   
+        st.session_state.period_start = month
+
+if "period_end" not in st.session_state :
+    st.session_state.period_end = st.session_state.period_start + relativedelta(months=1) 
 
 # endregion
 
@@ -42,11 +51,12 @@ if not DATA_PATH.exists() :
 
 ALL_USERS = [user_folder.stem for user_folder in DATA_PATH.iterdir() if user_folder.is_dir()]
 if not "user" in st.session_state :
-    st.session_state.user = ALL_USERS[0]
+    st.session_state.user = ALL_USERS[0] if len(ALL_USERS) > 0 else "default"
 
 USER_PATH = DATA_PATH / st.session_state.user
 if not USER_PATH.exists() :
     os.mkdir(USER_PATH)
+    ALL_USERS.append(st.session_state.user)
 
 # endregion
 
@@ -117,13 +127,45 @@ ALL_CATEGORIES = [Category.house, Category.eat, Category.pay]
 
 def display_settings() :
     
-    col_settings = st.columns(4)
+    col_settings = st.columns([1, 1, 1, 2, 1], vertical_alignment="bottom")
 
     user = col_settings[0].selectbox(
-        "Compte",
+        "Sélection du compte",
         options=ALL_USERS,
         key="user",
     )
+    with col_settings[1].popover("Nouveau compte", use_container_width=True) :
+        with st.form(key="new_user") :
+            new_user = st.text_input(
+                "Nouveau compte",
+                key="new_user",
+            )
+
+            new_user_button = st.form_submit_button("Créer")
+            if new_user_button :
+                # TODO message de confirmation
+                os.mkdir(DATA_PATH / new_user)
+                st.rerun()
+    
+    month = col_settings[2].date_input(
+        "Mois courant",
+        format="DD/MM/YYYY",
+        key="month",
+        on_change=...
+    )# TODO unifier avec l'init
+    
+    horizon = col_settings[3].slider(
+        "Horizon (mois)",
+        min_value=1,
+        max_value=12,
+        step=1,
+        key="horizon"
+    )
+
+    with col_settings[4].popover("Paramètres", use_container_width=True) :
+        ...
+        # TODO  
+
 
 # endregion
 
@@ -156,9 +198,12 @@ def display_ponctuals_editor() :
 def run_ui() :
 
     st.set_page_config(layout="wide")
-    st.title("Cabank")
 
-    with st.container(border=True) :
+    col_title = st.columns([2, 8])
+
+    col_title[0].title("Cabank")
+
+    with col_title[1].container(border=True) :
         display_settings()
     
     with st.container(border=True) :
@@ -173,7 +218,6 @@ def run_ui() :
 
 if __name__ == '__main__' :
     run_ui()
-    print(st.session_state.user)
 
 # endregion
 
