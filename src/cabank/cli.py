c8514b7a5d238a6e6a81864790894b928fe6e0e4
Arthur Cabon <djarth2222@gmail.com>
2026-02-04 19:00:35 +0100

    fix: stop using streamlit binary with subprocess

--- src/cabank/cli.py
+++ src/cabank/cli.py
@@ -1,7 +1,14 @@
-import subprocess
 from pathlib import Path
+import streamlit.web.bootstrap
+from importlib import resources
 
 def run():
     src_dir = Path(__file__).absolute().parent
     main_path = src_dir / "main.py"
-    subprocess.run(["streamlit", "run", main_path])
+    with resources.as_file(resources.files("cabank") / "main.py") as app_path:
+        streamlit.web.bootstrap.run(
+            main_script_path=str(app_path),
+            is_hello=False,
+            args=[],
+            flag_options={}
+        )
